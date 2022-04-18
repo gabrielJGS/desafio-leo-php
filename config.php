26ea1681f4c4ffb2e45ac498d14c1b9dfd190d10
Gabriel José <gabrielftjose@gmail.com>
2022-04-18 15:10:27 -0300

    create config database

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// host
+// db
+$db_name = 'desafioleo';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+} catch (\Throwable $th) {
+  throw $th;
+}
---
+++ create_database.sql
@@ -0,0 +1,18 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+  `link` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
