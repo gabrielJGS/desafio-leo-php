b8737f8300a65dfd2bf06dcb019c43adc737dcce
Gabriel José <gabrielftjose@gmail.com>
2022-04-18 15:12:32 -0300

    course controller

---
+++ src/controllers/course.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '../../database/course.php';
+
+function findCourseAction($conn, $id)
+{
+  return findCourseDb($conn, $id);
+}
+
+function readCourseAction($conn)
+{
+  return readCourseDb($conn);
+}
+
+function createCourseAction($conn, $title, $description, $image, $link)
+{
+  $createCourseDb = createCourseDb($conn, $title, $description, $image, $link);
+  $message = $createCourseDb == 1 ? 'success-create' : 'error-create';
+  return header("Location: ./read.php?message=$message");
+}
+
+function updateCourseAction($conn, $id, $title, $description, $image, $link)
+{
+  $updateCourseDb = updateCourseDb($conn, $id, $title, $description, $image, $link);
+  $message = $updateCourseDb == 1 ? 'success-update' : 'error-update';
+  return header("Location: ./read.php?message=$message");
+}
+
+function deleteCourseAction($conn, $id)
+{
+  $deleteCourseDb = deleteCourseDb($conn, $id);
+  $message = $deleteCourseDb == 1 ? 'success-remove' : 'error-remove';
+  return header("Location: ./read.php?message=$message");
+}
