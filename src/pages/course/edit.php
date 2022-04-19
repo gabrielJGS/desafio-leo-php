40ad9ae6608fff11e0534120c09a51badac13bcc
Gabriel José <gabrielftjose@gmail.com>
2022-04-18 22:34:22 -0300

    feature: edit courses

--- src/pages/course/edit.php
+++ src/pages/course/edit.php
@@ -4,13 +4,14 @@ require_once '../../../config.php';
 require_once '../../controllers/course.php';
 require_once '../partials/header.php';
 
-if (isset($_POST["id"], $_POST["title"]) && isset($_POST["description"]) && isset($_POST["image"]) && isset($_POST["link"]))
-  updateCourseAction($conn, $_POST["id"], $_POST["title"], $_POST["description"], $_POST["image"], $_POST["link"]);
-
+if (isset($_POST["id"], $_POST["title"]) && isset($_POST["description"]) && isset($_POST["image"])){
+  updateCourseAction($conn, $_POST["id"], $_POST["title"], $_POST["description"], $_POST["image"]);
+}
 $course = findCourseAction($conn, $_GET['id']);
 ?>
 <div class="container">
   <form action="./edit.php" method="POST">
+    <input type="hidden" name="id" value="<?= $course['id'] ?>" required />
     <div class="mb-3">
       <label for="title" class="form-label">Título</label>
       <input type="text" class="form-control" name="title" value="<?= $course['title'] ?>" required placeholder="Digite o título do curso">
